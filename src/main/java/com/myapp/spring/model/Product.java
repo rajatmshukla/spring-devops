860845980dc567ac86c5dfab37e1a4bd5e3429c8
rajatmshukla <rajatmshukla@gmail.com>
2021-02-23 14:45:38 +0530

    sonar integration

--- src/main/java/com/myapp/spring/model/Product.java
+++ src/main/java/com/myapp/spring/model/Product.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="newproducts")
+@Table(name = "newproducts")
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,36 +15,46 @@ public class Product {
 	private String productName;
 	private Double price;
 	private String description;
+
 	public Product() {
-		// TODO Auto-generated constructor stub
+
 	}
+
 	public Product(String productName, Double price, String description) {
 		super();
 		this.productName = productName;
 		this.price = price;
 		this.description = description;
 	}
+
 	public Integer getProductId() {
 		return productId;
 	}
+
 	public void setProductId(Integer productId) {
 		this.productId = productId;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public Double getPrice() {
 		return price;
 	}
+
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
